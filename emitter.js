5de2e112e79604548b63004305d17be90fe86432
Solodovnikova Ekaterina <colo18@yandex.ru>
2018-10-31 15:56:20 +0500

    inital commit

--- emitter.js
+++ emitter.js
@@ -11,63 +11,141 @@ const isStar = true;
  * @returns {Object}
  */
 function getEmitter() {
+    var events = {};
+    var currentTime = 0;
+
     return {
 
-        /**
-         * Подписаться на событие
-         * @param {String} event
-         * @param {Object} context
-         * @param {Function} handler
-         */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            if (event in events) {
+                events[event].push({
+                    data: context,
+                    action: handler,
+                    finishTime: Infinity,
+                    frequency: 1
+                });
+            } else {
+                events[event] = [{
+                    data: context,
+                    action: handler,
+                    finishTime: Infinity,
+                    frequency: 1
+                }];
+            }
+
+            // идемпотентность
+            return this;
         },
 
-        /**
-         * Отписаться от события
-         * @param {String} event
-         * @param {Object} context
-         */
         off: function (event, context) {
-            console.info(event, context);
+            const allEventsToOff = findAllSubEvents(event, events).concat(event);
+            allEventsToOff.forEach(function (e) {
+                const allContexts = events[e];
+                const subscriberIndex = findSubscriber(allContexts, context);
+                if (subscriberIndex !== -1) {
+                    events[e].splice(subscriberIndex, 1);
+                }
+            });
+            // console.info(event, context);
+
+            return this;
         },
 
-        /**
-         * Уведомить о событии
-         * @param {String} event
-         */
-        emit: function (event) {
-            console.info(event);
+
+        emit: function (event) { // check time
+            const allEvents = getAllEvents(event);
+            allEvents.forEach(function (e) {
+                const allSubscribers = events[e];
+                if (allSubscribers !== undefined) {
+                    allSubscribers.forEach(function (i) {
+                        const permissionToEmit = checkEvent(i, currentTime);
+                        if (permissionToEmit) {
+                            i.action.call(i.data);
+                        }
+                    });
+                }
+            });
+            if (event !== 'begin') {
+                currentTime++;
+            }
+            // console.info(event);
+
+            return this;
         },
 
-        /**
-         * Подписаться на событие с ограничением по количеству полученных уведомлений
-         * @star
-         * @param {String} event
-         * @param {Object} context
-         * @param {Function} handler
-         * @param {Number} times – сколько раз получить уведомление
-         */
         several: function (event, context, handler, times) {
+            if (times <= 0) { // case 0
+                this.on(event, context, handler);
+            }
+            const finishTime = currentTime + times;
+            this.on(event, context, handler);
+            events[event][findSubscriber(events[event], context)].finishTime = finishTime;
             console.info(event, context, handler, times);
+
+            return this;
         },
 
-        /**
-         * Подписаться на событие с ограничением по частоте получения уведомлений
-         * @star
-         * @param {String} event
-         * @param {Object} context
-         * @param {Function} handler
-         * @param {Number} frequency – как часто уведомлять
-         */
         through: function (event, context, handler, frequency) {
+            if (frequency <= 0) { // case 0
+                this.on(event, context, handler);
+            }
+            this.on(event, context, handler);
+            events[event][findSubscriber(events[event], context)].frequency = frequency;
             console.info(event, context, handler, frequency);
+
+            return this;
         }
     };
 }
 
+function checkEvent(event, currentTime) {
+    const isTimeAppropriate = event.finishTime > currentTime;
+    const isFrequencyAppropriate = currentTime % event.frequency === 0;
+
+    return isTimeAppropriate && isFrequencyAppropriate;
+}
+
+function findAllSubEvents(event, events) {
+    var result = [];
+    Object.keys(events).forEach(function (i) {
+        const a = i.slice(event.length);
+        if (a[0] === '.') {
+            result.push(i);
+        }
+    });
+
+    return result;
+}
+
+function findSubscriber(allContexts, context) {
+    for (var i = 0; i < allContexts.length; i++) {
+        const isWisdomSame = allContexts[i].data.wisdom === context.wisdom;
+        const isFocusSame = allContexts[i].data.focus === context.focus;
+
+        if (isFocusSame && isWisdomSame) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+function getAllEvents(event) {
+    const splittedEvent = event.split('.');
+    if (splittedEvent.length === 1) {
+        return [event];
+    }
+    var result = [splittedEvent[0]];
+    splittedEvent.reduce(function (prev, cur) {
+        result.push(prev + '.' + cur);
+
+        return prev + '.' + cur;
+    });
+
+    return result.reverse();
+}
+
 module.exports = {
     getEmitter,
-
     isStar
 };
